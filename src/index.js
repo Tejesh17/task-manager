c9ca8a92a57d5573313fae35f164a8dcfa45b6db
Tejesh Paidi <teju708@gmail.com>
2020-10-20 22:54:30 +0530

    init commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const request = require('request')
+require('./db/mongoose')
+
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express();
+
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+
+app.post('/users', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save().then(()=>{
+        res.status(201).send(user)
+    }).catch((error)=>{
+        res.status('400').send(error)
+    })
+
+
+})
+
+app.post('/tasks', (req, res) => {
+
+    const task = new Task(req.body)
+
+    task.status(201).save().then(()=>{
+        res.send(task)
+    }).catch((error)=>{
+        res.status('400').send(error)
+    })
+
+
+})
+
+
+
+app.listen(port, ()=>{
+    console.log("server is up on port "+ port)
+})
